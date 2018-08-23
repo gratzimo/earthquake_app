1e5678ad2097d037f17cff2e39a8e8d2088e0048
gratzimo <manyu.singh998@gmail.com>
2018-08-23 20:51:22 +0530

    First Commit v1.0

---
+++ app/src/main/java/com/gratzimo/quakereport/EarthquakeLoader.java
@@ -0,0 +1,43 @@
+package com.gratzimo.quakereport;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.gratzimo.quakereport.Earthquake;
+
+import java.util.List;
+
+/* Loads a list of earthquakes by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    /* Tag for log messages */
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    /* Query URL */
+    private String mUrl;
+
+    /*Constructs a new EarthquakeLoader. */
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /* This is on a background thread. */
+    @Override
+    public List<Earthquake> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<Earthquake> earthquakes = com.gratzimo.quakereport.QueryUtils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+}
